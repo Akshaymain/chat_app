d61f595966ba1f9bfe29f2977705dc7b1d6e6a5d
akshay.kumar <akshaymain95@gmail.com>
2021-06-07 17:48:35 +0530

    Event acknowledgements is covered

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,26 +1,35 @@
 const socket = io()
 
-socket.on('message',(message) => {
+socket.on('message', (message) => {
     console.log(message)
 })
 
-document.querySelector('#message-form').addEventListener('submit',(e) => {
+document.querySelector('#message-form').addEventListener('submit', (e) => {
     e.preventDefault()
 
     const message = e.target.elements.message.value
-    
-    socket.emit('sendMessage',message)
+
+    socket.emit('sendMessage', message, (error) => {
+
+        if (error) {
+            return console.log(error)
+        }
+
+        console.log('Message delivered!')
+    })
 })
 
 document.querySelector('#send-location').addEventListener('click', () => {
-    if(!navigator.geolocation){
+    if (!navigator.geolocation) {
         alert('Geolocation is not supported by your browser')
     }
 
     navigator.geolocation.getCurrentPosition((position) => {
-        socket.emit('sendLocation',{
+        socket.emit('sendLocation', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
+        },() => {
+            console.log('Location Shared!')
         })
     })
 })
