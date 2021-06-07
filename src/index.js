19bab1b34f2c6b8b83072d09f667378f8fadc394
akshay.kumar <akshaymain95@gmail.com>
2021-06-07 16:11:22 +0530

    1.Get user input and send to all users 2.Display user entered the chat 3.Display user left the chat

--- src/index.js
+++ src/index.js
@@ -8,21 +8,23 @@ const server = http.createServer(app)
 const io = socketio(server)
 
 const port = process.env.PORT || 3000
-const publicDirectoryPath = path.join(__dirname,'../public')
+const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
 
-let count = 0
-
-io.on('connection',(socket) => {
+io.on('connection', (socket) => {
     console.log('New websocket connection')
 
-    socket.emit('countUpdated',count)
+    socket.emit('message', 'Welcome!')
 
-    socket.on('increment',() => {
-        count++
-        //socket.emit('countUpdated',count)
-        io.emit('countUpdated',count)
+    socket.broadcast.emit('message', 'A new user has joined')
+
+    socket.on('sendMessage', (message) => {
+        io.emit('message', message)
+    })
+
+    socket.on('disconnect', () => {
+        io.emit('message','A user has left the chat!')
     })
 })
 
